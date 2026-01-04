93bec3d2d9afc96d53e5b513cddcdc2491a9c124
matheuscrluiz <matheuscrluiz@gmail.com>
2026-01-04 18:36:29 -0300

    feat: add crud for instituicao

--- controlei/model/facade/controlei_instituicao_facade.py
+++ controlei/model/facade/controlei_instituicao_facade.py
@@ -25,125 +25,39 @@ class ControleiInstituicaoFacade():
         except Exception as erro:
             raise FacadeException(__file__, rotina, erro)
 
-    def criar_investimento(self, parm_dict: dict):
-        rotina = 'criar_investimento'
+    def criar_instituicao(self, parm_dict: dict):
+        rotina = 'criar_instituicao'
 
         try:
-            obtem_categoria = convert_unique_dic_to_arrayDict(
-                self.cat_dao.get_category(
-                    id_categoria=parm_dict['id_categoria']
-                ))
 
-            if not obtem_categoria:
-                raise FacadeException(
-                    __file__,
-                    rotina,
-                    "Categoria não encontrada!"
-                )
-
-            print('obtem_categoria: ', obtem_categoria)
-            if obtem_categoria[0][
-                    'codigo_tipo_categoria'] != 'Investimentos':
-                raise FacadeException(
-                    __file__,
-                    rotina,
-                    "A categoria deve ser do tipo investimento"
-                )
-
-            if parm_dict['valor_inicial'] < 0:
-                raise FacadeException(
-                    __file__,
-                    rotina,
-                    "O valor da investimento não pode ser menor que zero"
-                )
-
-            id_investimento = self.dao.insert_investment(parm_dict)
+            id_instituicao = self.dao.insert_banks(parm_dict)
             self.dao.database_commit()
 
-            return id_investimento
+            return id_instituicao
 
         except Exception as erro:
             raise FacadeException(__file__, rotina, erro)
 
-    def atualizar_investimento(self, parm_dict: dict):
-        rotina = 'atualizar_investimento'
+    def atualizar_instituicao(self, parm_dict: dict):
+        rotina = 'atualizar_instituicao'
 
         try:
-            if not parm_dict.get('id_investimento'):
-                raise FacadeException(
-                    __file__, rotina, 'ID da investimento é obrigatório'
-                )
 
-            investimento = self.dao.get_investment(
-                id_investimento=parm_dict['id_investimento'],
-                ch_rede=parm_dict['ch_rede'])
-            if not investimento:
-                raise FacadeException(
-                    __file__, rotina, 'investimento não encontrada'
-                )
-
-            obtem_categoria = convert_unique_dic_to_arrayDict(
-                self.cat_dao.get_category(
-                    id_categoria=parm_dict['id_categoria']
-                )
-            )
-
-            if not obtem_categoria:
-                raise FacadeException(
-                    __file__,
-                    rotina,
-                    "Categoria não encontrada!"
-                )
-
-            if parm_dict['valor_inicial'] < 0:
-                raise FacadeException(
-                    __file__,
-                    rotina,
-                    "O valor da investimento não pode ser menor que zero"
-                )
-
-            self.dao.update_investment(parm_dict)
+            self.dao.update_banks(parm_dict)
             self.dao.database_commit()
 
         except Exception as erro:
             raise FacadeException(__file__, rotina, erro)
 
-    def apagar_investimento(
+    def apagar_instituicao(
             self,
             id_investimento: int,
-            ch_rede: str
     ):
-        rotina = 'apagar_investimento'
+        rotina = 'apagar_instituicao'
 
         try:
-            if not id_investimento:
-                raise FacadeException(
-                    __file__, rotina, 'ID da investimento é obrigatório'
-                )
 
-            investimento = self.dao.get_investment(
-                id_investimento=id_investimento,
-                ch_rede=ch_rede)
-            if not investimento:
-                raise FacadeException(
-                    __file__, rotina, 'investimento não encontrada'
-                )
-
-            usuario = self.user_dao.get_user(
-                ch_rede=ch_rede)
-
-            if not usuario:
-                raise FacadeException(
-                    __file__, rotina, 'Usuário não encontrado'
-                )
-            if usuario[0]['ch_rede'] != ch_rede:
-                raise FacadeException(
-                    __file__,
-                    rotina,
-                    'Você não tem permissão para deletar esta investimento'
-                )
-
-            self.dao.delete_investment(id_investimento, ch_rede)
+            self.dao.delete_banks(id_investimento)
             self.dao.database_commit()
 
         except Exception as erro:
