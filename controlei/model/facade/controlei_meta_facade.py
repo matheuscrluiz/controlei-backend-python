6d3fe08abb1c181ea9128200a3f2a2006f58c6ed
matheuscrluiz <matheuscrluiz@gmail.com>
2026-01-10 10:53:27 -0300

    feat: add goals flow

---
+++ controlei/model/facade/controlei_meta_facade.py
@@ -0,0 +1,136 @@
+from ...util.exceptions import FacadeException
+from ...util.util import convert_unique_dic_to_arrayDict
+from ..dao.controlei_meta_dao import ControleiMetaDAO
+from ..dao.controlei_usuario_dao import ControleiUserDAO
+
+
+class ControleiMetaFacade():
+
+    def __init__(self):
+        """construtor da classe ControleiUserFacade"""
+        self.dao = ControleiMetaDAO()
+        self.user_dao = ControleiUserDAO()
+
+    def obter_meta(
+        self,
+        id_meta=None,
+        ch_rede=None,
+    ) -> dict:
+        rotina = 'obter_meta'
+
+        try:
+
+            ch_rede = ch_rede.upper()
+
+            meta = self.dao.get_goals(
+                id_meta=id_meta,
+                ch_rede=ch_rede,
+            )
+            return convert_unique_dic_to_arrayDict(meta)
+
+        except Exception as erro:
+            raise FacadeException(__file__, rotina, erro)
+
+    def criar_meta(self, parm_dict: dict):
+        rotina = 'criar_meta'
+
+        try:
+            parm_dict['ch_rede'] = parm_dict['ch_rede'].upper()
+
+            if parm_dict['valor_meta'] < 0:
+                raise FacadeException(
+                    __file__,
+                    rotina,
+                    "O valor da meta não pode ser menor que zero"
+                )
+
+            id_meta = self.dao.insert_goals(parm_dict)
+            self.dao.database_commit()
+
+            return id_meta
+
+        except Exception as erro:
+            raise FacadeException(__file__, rotina, erro)
+
+    def atualizar_meta(self, parm_dict: dict):
+        rotina = 'atualizar_meta'
+
+        try:
+            if not parm_dict.get('id_meta'):
+                raise FacadeException(
+                    __file__, rotina, 'ID da meta é obrigatório'
+                )
+
+            meta = self.dao.get_goals(
+                id_meta=parm_dict['id_meta'],
+                ch_rede=parm_dict['ch_rede'])
+            if not meta:
+                raise FacadeException(
+                    __file__, rotina, 'meta não encontrada'
+                )
+
+            if parm_dict['valor_meta'] < 0:
+                raise FacadeException(
+                    __file__,
+                    rotina,
+                    "O valor da meta não pode ser menor que zero"
+                )
+
+            self.dao.update_goals(parm_dict)
+            self.dao.database_commit()
+
+        except Exception as erro:
+            raise FacadeException(__file__, rotina, erro)
+
+    def apagar_meta(
+            self,
+            id_meta: int,
+            ch_rede: str
+    ):
+        rotina = 'apagar_meta'
+
+        try:
+            if not id_meta:
+                raise FacadeException(
+                    __file__, rotina, 'ID da meta é obrigatório'
+                )
+
+            meta = self.dao.get_goals(
+                id_meta=id_meta,
+                ch_rede=ch_rede)
+            if not meta:
+                raise FacadeException(
+                    __file__, rotina, 'meta não encontrada'
+                )
+
+            usuario = self.user_dao.get_user(
+                ch_rede=ch_rede)
+
+            if not usuario:
+                raise FacadeException(
+                    __file__, rotina, 'Usuário não encontrado'
+                )
+            if usuario[0]['ch_rede'] != ch_rede:
+                raise FacadeException(
+                    __file__,
+                    rotina,
+                    'Você não tem permissão para deletar esta meta'
+                )
+
+            self.dao.delete_goals(id_meta, ch_rede)
+            self.dao.database_commit()
+
+        except Exception as erro:
+            raise FacadeException(__file__, rotina, erro)
+
+    def obter_relatorio_saldo_mensal(self, ch_rede: str) -> dict:
+        rotina = 'obter_relatorio_saldo_mensal'
+
+        try:
+            ch_rede = ch_rede.upper()
+
+            relatorio = self.dao.get_monthly_balance_report(ch_rede=ch_rede)
+            return convert_unique_dic_to_arrayDict(relatorio)
+
+        except Exception as erro:
+            raise FacadeException(__file__, rotina, erro)
---
+++ controlei/view/api/model/controlei_meta_model.py
@@ -0,0 +1,38 @@
+from flask_restx import Model, fields, Namespace
+
+
+def generate_meta_model(api: Namespace, type: str) -> Model:
+    model = {
+        'ch_rede': fields.String(
+            required=True,
+            description="Chave de rede do usuário"
+        ),
+        'dsc_meta': fields.String(
+            required=False,
+            description="Descrição da meta"
+        ),
+        'valor_meta': fields.Float(
+            required=True,
+            description="Valor da meta"
+        ),
+        'prioridade': fields.Integer(
+            required=False,
+            description="Prioridade da meta"
+        ),
+        'ativa': fields.String(
+            required=False,
+            description="Se meta está ativa"
+        ),
+    }
+
+    if type == 'post':
+        return api.model(name='post_meta_model', model=model)
+
+    model.update({
+        'id_meta': fields.Integer(
+            required=True,
+            description="ID da meta"
+        ),
+    })
+
+    return api.model(name='put_meta_model', model=model)
